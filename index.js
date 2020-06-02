dd6561e1022aaa59516661e0f873313816737ea2
Aidan Q <aidanq52@gmail.com>
2020-06-01 17:06:46 -0700

    need to add a badge and figure out table of contents

--- index.js
+++ index.js
@@ -16,28 +16,49 @@ const questions = [
         message: "Give a description of this project:",
         name: "description"
     },
+    
+    // {
+    //     type: "input",
+    //     message: "table of contents:",
+    //     name: "contents"
+    // },
 
-    // // {
-    // //     type: "input",
-    // //     message: "Enter a title:",
-    // //     name: "title"
-    // // },
-    // // {
-    // //     type: "input",
-    // //     message: "Enter a title:",
-    // //     name: "title"
-    // // },
-    // // {
-    // //     type: "input",
-    // //     message: "Enter a title:",
-    // //     name: "title"
-    // // },
-    // // {
-    // //     type: "input",
-    // //     message: "Enter a title:",
-    // //     name: "title"
-    // // },
+    {
+        type: "input",
+        message: "Enter installation information:",
+        name: "installation"
+    },
 
+    {
+        type: "input",
+        message: "Enter usage information:",
+        name: "usage"
+    },
+
+    {
+        type: "input",
+        message: "Enter license information:",
+        name: "license"
+    },
+
+    {
+        type: "input",
+        message: "How many people contributed:",
+        name: "constribute"
+    },
+
+    {
+        type: "input",
+        message: "Tests:",
+        name: "tests"
+    },
+
+    {
+        type: "input",
+        message: "Questions to list in ReadMe:",
+        name: "question"
+    },
+    
 ];
 
 function writeToFile(fileName, data) {
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,22 @@
 function generateMarkdown(data) {
   return `
 ${generateTitle( data.title )}
+## Description
 ${generateDescription( data.description)}
+## Installation information:
+${generateInstallation(data.installation)}
+## Usage Information:
+${generateUsage(data.usage)}
+## License Information:
+${generateLicense(data.license)}
 
+## Contribution Information:
+${generateContribution(data.contribution)}
+
+## Testing Information:
+${generateTests(data.tests)}
+## Questions:
+${generateQuestions(data.questions)}
 `;
 }
 
@@ -16,5 +30,30 @@ function generateDescription (description) {
   `;
 } 
 
+function generateInstallation (installation) {
+  return `${installation}
+  `;
+} 
+
+function generateUsage (usage) {
+  return `${usage}
+  `;
+} 
+function generateLicense (license) {
+  return `${license}
+  `;
+} 
+function generateContribution (contribution) {
+  return `${contribution}
+  `;
+} 
+function generateTests(tests) {
+  return `${tests}
+  `;
+} 
+function generateQuestions (questions) {
+  return `${questions}
+  `;
+} 
 
 module.exports = generateMarkdown;
